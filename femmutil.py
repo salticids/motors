89b5467542598e835c391d59c463b04245f1736a
salticids <graydon@seewasp.com>
2019-03-01 19:16:59 -0800

    Reorganized functions; added single-layer winding function
    Moved drawing functions into femmutil.py. Motor parameters are passed in as arguments; drawing rotor is made independent of drawing stator. Single layer winding function added, indexing is still buggy. Circuit/material adding is still done in pmsm.py

---
+++ femmutil.py
@@ -0,0 +1,231 @@
+import femm
+import cmath as cm
+# from materials import *
+from math import pi, asin
+
+def rad(x):
+    return x * pi / 180.
+
+def addNode(r, phi):
+    n = cm.rect(r, rad(phi))
+    femm.mi_addnode(n.real, n.imag)
+
+def addBlockLabel(r, phi):
+    n = cm.rect(r, rad(phi))
+    femm.mi_addblocklabel(n.real, n.imag)
+
+# draw arc using polar coordinates (deg)
+def addArc(r1, phi1, r2, phi2):
+    n1 = cm.rect(r1, rad(phi1))
+    n2 = cm.rect(r2, rad(phi2))
+    femm.mi_addarc(n1.real, n1.imag, n2.real, n2.imag, abs(phi1-phi2), 10)
+
+# draw line using polar coordinates
+def addLine(r1, phi1, r2, phi2):
+    n1 = cm.rect(r1, rad(phi1))
+    n2 = cm.rect(r2, rad(phi2))
+    femm.mi_addsegment(n1.real, n1.imag, n2.real, n2.imag)
+
+def setMat(r, phi, mat):
+    n = cm.rect(r, rad(phi))
+    femm.mi_selectlabel(n.real, n.imag)
+    femm.mi_setblockprop(mat, 1, 0, '<None>', 0, 0, 0)
+    femm.mi_clearselected()
+
+def setCirc(r, phi, mat, circuit, turns):
+    n = cm.rect(r, rad(phi))
+    femm.mi_selectlabel(n.real, n.imag)
+    femm.mi_setblockprop(mat, 1, 0, circuit, 0, 0, turns)
+    femm.mi_clearselected()
+
+def setMagnet(r, phi, mat, magdir):
+    n = cm.rect(r, rad(phi))
+    femm.mi_selectlabel(n.real, n.imag)
+    femm.mi_setblockprop(mat, 1, 0, '<None>', magdir, 0, 0)
+    femm.mi_clearselected()
+
+# Number of magnets, 
+# fractional magnet width,
+# shaft radius,
+# rotor body radius, 
+# height of magnets,
+# rotor material,
+# magnet material
+# array of magnet block labels
+def rotor(Nm, Mfrac, rsh, rr, hm, matRotor, matMagnet, magnetBlockLabels):
+
+    # find pitch of magnets
+    pitchMagnet = 360. * Mfrac / Nm
+
+    # shaft
+    femm.mi_drawarc(rsh, 0, -rsh, 0, 180, 10)
+    femm.mi_drawarc(-rsh, 0, rsh, 0, 180, 10)
+    # rotor
+    femm.mi_drawarc(rr, 0, -rr, 0, 180, 10)
+    femm.mi_drawarc(-rr, 0, rr, 0, 180, 10)
+
+    # draw rotor magnets
+    for i in range(Nm):
+        # magnet center position
+        phi = 360 * i / Nm
+        # ccw magnet edge
+        phi_plus = phi + pitchMagnet / 2.
+        # cw magnet edge
+        phi_minus = phi - pitchMagnet / 2.
+
+        # add block label to magnet
+        magnetBlockLabels.append([(rr + rr + hm) / 2., phi])
+        addBlockLabel(magnetBlockLabels[len(magnetBlockLabels)-1][0],magnetBlockLabels[len(magnetBlockLabels)-1][1])
+        if i % 2 == 0:
+            setMagnet(magnetBlockLabels[len(magnetBlockLabels)-1][0],magnetBlockLabels[len(magnetBlockLabels)-1][1], matMagnet, phi)
+        else:
+            setMagnet(magnetBlockLabels[len(magnetBlockLabels)-1][0],magnetBlockLabels[len(magnetBlockLabels)-1][1], matMagnet, phi - 180.)
+
+        
+        r = rr + hm
+        z = cm.rect(r, rad(phi))
+        z1 = cm.rect(r, rad(phi_minus))
+        z1s = cm.rect(rr, rad(phi_minus))
+        z2 = cm.rect(r, rad(phi_plus))
+        z2s = cm.rect(rr, rad(phi_plus))
+        femm.mi_addnode(z.real, z.imag)
+        femm.mi_addnode(z1s.real, z1s.imag)
+        femm.mi_addnode(z2s.real, z2s.imag)
+        femm.mi_drawarc(z1.real, z1.imag, z2.real, z2.imag, pitchMagnet, 10)
+        femm.mi_addsegment(z1s.real, z1s.imag, z1.real, z1.imag)
+        femm.mi_addsegment(z2s.real, z2s.imag, z2.real, z2.imag)
+
+    # shaft block label
+    if(rsh > 0):
+        addBlockLabel(0, 0)
+        setMat(0, 0, 'Air')
+    # rotor body block label
+    addBlockLabel((rsh + rr) / 2., 0)
+    setMat((rsh + rr) / 2., 0, 'M-27 Steel')
+
+
+# number of teeth,
+# fractional tooth pitch
+# radius to inner tooth surface,
+# radial thickness of tooth
+# thickness of root
+# slot height
+# stator radial thickness
+# array of coil block labels
+def stator(Nt, Tfrac, rt, ht, bt, hs, hstator, coilBlockLabels):
+
+    # radius to outer tooth surface
+    rto = rt + ht
+    # radius to inner stator surface
+    rsi = rto + hs
+    # total machine radius
+    rstator = rsi + hstator
+
+    # find angle between teeth
+    phiBetweenTeeth = (360. / Nt) * (1. - Tfrac)
+
+    # find angle between roots 
+    phiRoot = asin((bt / 2.)/rsi) * 180. / pi
+    phiBetweenRoots = (360. / Nt) - (2. * phiRoot)
+
+
+    # draw teeth
+    for i in range(Nt):
+        phi = 360 * i / Nt
+        pitchTooth = 360 * (Tfrac) / Nt
+        phi_plus = phi + pitchTooth / 2.
+        phi_minus = phi - pitchTooth / 2.
+        r = rt
+        z = cm.rect(rt, rad(phi))
+        z1 = cm.rect(rt, rad(phi_minus))
+        z1s = cm.rect(rt + ht, rad(phi_minus))
+        z2 = cm.rect(rt, rad(phi_plus))
+        z2s = cm.rect(rt + ht, rad(phi_plus))
+        femm.mi_addnode(z.real, z.imag)
+        femm.mi_addnode(z1s.real, z1s.imag)
+        femm.mi_addnode(z2s.real, z2s.imag)
+        femm.mi_drawarc(z1.real, z1.imag, z2.real, z2.imag, pitchTooth, 10)
+        femm.mi_addsegment(z1s.real, z1s.imag, z1.real, z1.imag)
+        femm.mi_addsegment(z2s.real, z2s.imag, z2.real, z2.imag)
+
+        phi_tooth = asin((bt/2.)/(rt+ht)) * 180. / pi
+        ztoothccw = cm.rect(rt+ht, rad(phi+phi_tooth))
+        ztoothcw = cm.rect(rt+ht, rad(phi-phi_tooth))
+        femm.mi_addnode(ztoothccw.real, ztoothccw.imag)
+        femm.mi_addnode(ztoothcw.real, ztoothcw.imag)
+
+        femm.mi_addarc(z1s.real, z1s.imag, ztoothcw.real, ztoothcw.imag, (pitchTooth - phi_tooth) / 2., 10)
+        femm.mi_addarc(ztoothccw.real, ztoothccw.imag, z2s.real, z2s.imag, (pitchTooth - phi_tooth) / 2., 10)
+
+        phi_toothbase = asin((bt/2.)/(rt+ht+hs)) * 180. / pi
+        ztoothbasecw = cm.rect(rt+ht+hs, rad(phi - phi_toothbase))
+        ztoothbaseccw = cm.rect(rt+ht+hs, rad(phi + phi_toothbase))
+        femm.mi_addnode(ztoothbasecw.real, ztoothbasecw.imag)
+        femm.mi_addnode(ztoothbaseccw.real, ztoothbaseccw.imag)
+
+        femm.mi_drawline(ztoothcw.real, ztoothcw.imag, ztoothbasecw.real, ztoothbasecw.imag)
+        femm.mi_drawline(ztoothccw.real, ztoothccw.imag, ztoothbaseccw.real, ztoothbaseccw.imag)
+
+    # Draw coil regions & stator zone
+    for i in range(Nt):
+
+        # offset to the space between teeth
+        phi = (360. * i + 180.) / Nt
+
+        # add nodes for interior coil boundary
+        addNode(rto, phi)
+        # add nodes for exterior coil boundary (abutt stator)
+        addNode(rsi, phi)
+        # draw coil separator between teeth
+        addLine(rto, phi, rsi, phi)
+
+        ### connect coil regions to teeth
+        # add clockwise connection
+        addArc(rto, phi - (phiBetweenTeeth / 2.), rto, phi)
+        # add counterclockwise connection
+        addArc(rto, phi, rto, phi + (phiBetweenTeeth / 2.))
+
+        ### draw stator interior
+        # draw clockwise connection
+        addArc(rsi, phi - (phiBetweenRoots / 2.), rsi, phi)
+        # draw counterclockwise connection
+        addArc(rsi, phi, rsi, phi + (phiBetweenRoots / 2.))
+
+        ### add block labels
+        # clockwise segment
+        coilBlockLabels.append([(rto + rsi) / 2., (phi + (phi - (phiBetweenRoots / 2.))) / 2.])
+        addBlockLabel(coilBlockLabels[len(coilBlockLabels)-1][0], coilBlockLabels[len(coilBlockLabels)-1][1])
+        #setCirc(coilBlockLabels[len(coilBlockLabels)-1][0], coilBlockLabels[len(coilBlockLabels)-1][1], '18 AWG', 'B', 1)
+        # counterclockwise segment
+        coilBlockLabels.append([(rto + rsi) / 2., (phi + (phi + (phiBetweenRoots / 2.))) / 2.])
+        addBlockLabel(coilBlockLabels[len(coilBlockLabels)-1][0], coilBlockLabels[len(coilBlockLabels)-1][1])
+        #setCirc(coilBlockLabels[len(coilBlockLabels)-1][0], coilBlockLabels[len(coilBlockLabels)-1][1], '18 AWG', 'B', 1)
+
+    # draw stator outside
+    femm.mi_drawarc(rstator, 0, -rstator, 0, 180, 10)
+    femm.mi_drawarc(-rstator, 0, rstator, 0, 180, 10)
+
+    # stator block label
+    addBlockLabel((rsi + rstator) / 2., 0)
+    setMat((rsi+rstator) / 2., 0, 'M-27 Steel')
+    # beyond stator 
+    addBlockLabel(rstator + 10., 0)
+    setMat(rstator + 10., 0, 'Air')
+
+### Label Air Gap
+# ri = outermost rotor surface radius
+# ro = innermost stator surface radius
+def airgap(ri, ro):
+    # air gap block label
+    addBlockLabel((ri + ro) / 2., 0)
+    setMat((ri + ro) / 2., 0, 'Air')
+
+### Single Layer Winding
+def singlelayerwind(poles, windings, circuits, coilBlockLabels):
+    polarity = 1
+    slots = len(coilBlockLabels) / 2
+    for index in range(int(len(coilBlockLabels) / 2)):
+        coil = int(((slots + 1 - index) % slots) / 2)
+        setCirc(coilBlockLabels[2*index][0], coilBlockLabels[2*index][1], '18 AWG', circuits[coil], windings*polarity)
+        setCirc(coilBlockLabels[2*index+1][0], coilBlockLabels[2*index+1][1], '18 AWG', circuits[coil], windings*polarity)
+        polarity = polarity * (-1)
--- pmsm.py
+++ pmsm.py
@@ -1,169 +1,55 @@
 import femm
+from femmutil import *
 from materials import *
-import cmath as cm
 from math import pi, asin
 
-def rad(x):
-    return x * pi / 180.
+depth = 50 # depth into plane (mm)
 
-def addNode(r, phi):
-    n = cm.rect(r, rad(phi))
-    femm.mi_addnode(n.real, n.imag)
+femm.openfemm()
+femm.newdocument(0) # magnetics problem
+femm.mi_probdef(0, 'millimeters', 'planar', 1.e-8, depth, 30)
 
-# draw arc using polar coordinates (deg)
-def addArc(r1, phi1, r2, phi2):
-    n1 = cm.rect(r1, rad(phi1))
-    n2 = cm.rect(r2, rad(phi2))
-    femm.mi_addarc(n1.real, n1.imag, n2.real, n2.imag, abs(phi1-phi2), 10)
-
-# draw line using polar coordinates
-def addLine(r1, phi1, r2, phi2):
-    n1 = cm.rect(r1, rad(phi1))
-    n2 = cm.rect(r2, rad(phi2))
-    femm.mi_addsegment(n1.real, n1.imag, n2.real, n2.imag)
+addMaterial(mat_Air)
+addMaterial(mat_18AWG)
+addMaterial(mat_NdFeB52MGOe)
+addMaterial(mat_M27Steel)
+femm.mi_addcircprop('A', 20, 1)
+femm.mi_addcircprop('B', 0, 1)
+femm.mi_addcircprop('C', -20, 1)
+circuits = ['A', 'B', 'C']
 
 ### PARAMETERS
+
+Nm = 18 # number of rotor magnets
 rsh = 10. # shaft radius (mm)
 rr = 40. # rotor inner radius (mm)
 delta = 5. # air gap
-Nm = 6 # number of rotor magnets
 Mfrac = 0.8
 hm = 10.
 
-Nt = 50 # number of teeth
-Ns = Nt # number of slots
+Nt = 20 # number of teeth; number of slots
 Tfrac = 0.8
 ht = 5. # tooth height
-bt = 2. # tooth thickness
-wRoot = bt # root width
+bt = 10. # root width
 hs = 20. # slot height
+hstator = 30 # stator thickness
 
-rstator = 100. # total machine radius
-
+rm = rr + hm # radius to outer magnet surface
 rti = rr + hm + delta # radius to inner tooth surface
 rto = rti + ht # radius to outer tooth surface
 rsi = rto + hs # radius to inner stator surface
 
-# find angle between teeth
-phiBetweenTeeth = (360. / Nt) * (1. - Tfrac)
+coilBlockLabels = []
+magnetBlockLabels = []
 
-# find angle between roots 
-phiRoot = asin((wRoot / 2.)/rsi)
-phiBetweenRoots = (360. / Nt) - (2. * phiRoot)
-
-# find pitch of magnets
-pitchMagnet = 360. * Mfrac / Nm
-# find pitch of teeth
-pitchTooth = 360. * Tfrac / Nm
-
-femm.openfemm()
-
-femm.newdocument(0); # magnetics problem
-femm.mi_probdef(0, 'millimeters', 'planar', 1.e-8, 0, 30)
-
-# draw rotor
-femm.mi_drawarc(rsh, 0, -rsh, 0, 180, 10)
-femm.mi_drawarc(-rsh, 0, rsh, 0, 180, 10)
-femm.mi_drawarc(rr, 0, -rr, 0, 180, 10)
-femm.mi_drawarc(-rr, 0, rr, 0, 180, 10)
-
-# draw rotor magnets
-for i in range(Nm):
-    # magnet center position
-    phi = 360 * i / Nm
-    # ccw magnet edge
-    phi_plus = phi + pitchMagnet / 2.
-    # cw magnet edge
-    phi_minus = phi - pitchMagnet / 2.
-    
-    r = rr + hm
-    z = cm.rect(r, rad(phi))
-    z1 = cm.rect(r, rad(phi_minus))
-    z1s = cm.rect(rr, rad(phi_minus))
-    z2 = cm.rect(r, rad(phi_plus))
-    z2s = cm.rect(rr, rad(phi_plus))
-    femm.mi_addnode(z.real, z.imag)
-    femm.mi_addnode(z1s.real, z1s.imag)
-    femm.mi_addnode(z2s.real, z2s.imag)
-    femm.mi_drawarc(z1.real, z1.imag, z2.real, z2.imag, pitchMagnet, 10)
-    femm.mi_addsegment(z1s.real, z1s.imag, z1.real, z1.imag)
-    femm.mi_addsegment(z2s.real, z2s.imag, z2.real, z2.imag)
-
-# draw teeth
-for i in range(Nt):
-    phi = 360 * i / Nt
-    pitchTooth = 360 * (Tfrac) / Nt
-    phi_plus = phi + pitchTooth / 2.
-    phi_minus = phi - pitchTooth / 2.
-    r = rr + hm + delta
-    z = cm.rect(r, rad(phi))
-    z1 = cm.rect(r, rad(phi_minus))
-    z1s = cm.rect(r + ht, rad(phi_minus))
-    z2 = cm.rect(r, rad(phi_plus))
-    z2s = cm.rect(r + ht, rad(phi_plus))
-    femm.mi_addnode(z.real, z.imag)
-    femm.mi_addnode(z1s.real, z1s.imag)
-    femm.mi_addnode(z2s.real, z2s.imag)
-    femm.mi_drawarc(z1.real, z1.imag, z2.real, z2.imag, pitchTooth, 10)
-    femm.mi_addsegment(z1s.real, z1s.imag, z1.real, z1.imag)
-    femm.mi_addsegment(z2s.real, z2s.imag, z2.real, z2.imag)
-
-    phi_tooth = asin((bt/2.)/(r+ht)) * 180. / pi
-    ztoothccw = cm.rect(r+ht, rad(phi+phi_tooth))
-    ztoothcw = cm.rect(r+ht, rad(phi-phi_tooth))
-    femm.mi_addnode(ztoothccw.real, ztoothccw.imag)
-    femm.mi_addnode(ztoothcw.real, ztoothcw.imag)
-
-    femm.mi_addarc(z1s.real, z1s.imag, ztoothcw.real, ztoothcw.imag, (pitchTooth - phi_tooth) / 2., 10)
-    femm.mi_addarc(ztoothccw.real, ztoothccw.imag, z2s.real, z2s.imag, (pitchTooth - phi_tooth) / 2., 10)
-
-    phi_toothbase = asin((bt/2.)/(r+ht+hs)) * 180. / pi
-    ztoothbasecw = cm.rect(r+ht+hs, rad(phi - phi_toothbase))
-    ztoothbaseccw = cm.rect(r+ht+hs, rad(phi + phi_toothbase))
-    femm.mi_addnode(ztoothbasecw.real, ztoothbasecw.imag)
-    femm.mi_addnode(ztoothbaseccw.real, ztoothbaseccw.imag)
-
-    femm.mi_drawline(ztoothcw.real, ztoothcw.imag, ztoothbasecw.real, ztoothbasecw.imag)
-    femm.mi_drawline(ztoothccw.real, ztoothccw.imag, ztoothbaseccw.real, ztoothbaseccw.imag)
-
-# Draw coil regions & stator zone
-for i in range(Nt):
-
-    # offset to the space between teeth
-    phi = (360. * i + 180.) / Nt
-
-    # add nodes for interior coil boundary
-    addNode(rto, phi)
-    # add nodes for exterior coil boundary (abutt stator)
-    addNode(rsi, phi)
-    # draw coil separator between teeth
-    addLine(rto, phi, rsi, phi)
-
-    ### connect coil regions to teeth
-    # add clockwise connection
-    addArc(rto, phi - (phiBetweenTeeth / 2.), rto, phi)
-    # add counterclockwise connection
-    addArc(rto, phi, rto, phi + (phiBetweenTeeth / 2.))
-
-    ### draw stator interior
-    # draw clockwise connection
-    addArc(rsi, phi - (phiBetweenRoots / 2.), rsi, phi)
-    # draw counterclockwise connection
-    addArc(rsi, phi, rsi, phi + (phiBetweenRoots / 2.))
-    
-
-
-# draw stator outside
-femm.mi_drawarc(rstator, 0, -rstator, 0, 180, 10)
-femm.mi_drawarc(-rstator, 0, rstator, 0, 180, 10)
-
-addMaterial(mat_Air)
-femm.mi_addblocklabel(0, 0)
-femm.mi_selectlabel(0, 0)
-femm.mi_setblockprop('Air', 0, 1, '<None>', 0, 0, 0)
-
+rotor(4, Mfrac, rsh, rr, hm, 'M-27 Steel', 'NdFeB 52 MGOe', magnetBlockLabels)
+stator(6, Tfrac, rti, ht, bt, hs, hstator, coilBlockLabels)
+airgap(rr+hm, rti)
+singlelayerwind(3, 100, circuits, coilBlockLabels)
 
 femm.mi_makeABC()
+
+femm.main_maximize()
 
 femm.mi_zoomnatural()
 
